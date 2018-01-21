097b73c6d46cf2be226b770ef691791dc0452650
MarkWeng <bskin0330@gmail.com>
2018-01-21 22:03:31 +0800

    renew index.html, add input_time_number

--- js/index.js
+++ js/index.js
@@ -5,15 +5,26 @@ function togglelight() {
   image.src = state? "img/on.jpg" : "img/off.jpg";
 }
 
-var timeLeft = 5;
- 
-function countDownTimer() {
-  timeLeft = timeLeft - 1;
+function input_time_number() {
+  var time_input = document.getElementById("time_input").value;
+  if (time_input == ""){
+    alert ("不能是空的");
+  } else if (isNaN(time_input)) {
+    alert ("請輸入數字");
+  } else {
+   var timeLeft = parseInt(time_input);    
+  }
+  
+ function countDownTimer() {
+   timeLeft = timeLeft - 1;
   if (timeLeft <= 0) {
     document.getElementById("image").src = "img/off.jpg";
     clearInterval(setting);
   }
 
-  document.getElementById("timer").innerHTML = timeLeft;       
+  document.getElementById("timer").innerHTML = timeLeft; 
+   
 }
 var setting =  setInterval(countDownTimer,1000);
+}
+
